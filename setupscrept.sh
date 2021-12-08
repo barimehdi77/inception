22bab14ec4c09742cad605cc3eccef2dd8017984
BariMehdi77 <barimehdi77@example.com>
2021-12-08 16:01:25 +0100

    add installion script

---
+++ setupscrept.sh
@@ -0,0 +1,12 @@
+sudo apt-get update -y
+sudo apt install openssh-server -y
+sudo apt install git -y
+sudo apt-get install     ca-certificates     curl     gnupg     lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null -y
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose -y
+sudo chmod +x /usr/local/bin/docker-compose -y
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose -y
