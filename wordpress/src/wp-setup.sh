0f6dd253bd1067ba1e0ae0d72d5d168c1fda0d29
BariMehdi77 <barimehdi77@example.com>
2021-12-09 17:23:03 +0100

    clean repo

--- wordpress/src/wp-setup.sh
+++
@@ -1,7 +0,0 @@
-cd /var/www/localhost/htdocs
-
-wget https://wordpress.org/latest.tar.gz
-tar xvf latest.tar.gz
-rm latest.tar.gz
-mv /tmp/wp-config.php /var/www/localhost/htdocs/wordpress/wp-config.php
-chown -R nginx:nginx /var/www/
