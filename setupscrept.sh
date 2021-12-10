1e9e886d1bdab7701396cd0fa746baa939b0e5cd
BariMehdi77 <barimehdi77@example.com>
2021-12-10 14:19:26 +0100

    now script ready to use

--- setupscrept.sh
+++ setupscrept.sh
@@ -15,7 +15,7 @@ echo "██████╔╝███████║██████╔╝�
 echo "██╔══██╗██╔══██║██╔══██╗██║    ██║╚██╔╝██║██╔══╝  ██╔══██║██║  ██║██║  ██╔╝    ██╔╝"
 echo "██████╔╝██║  ██║██║  ██║██║    ██║ ╚═╝ ██║███████╗██║  ██║██████╔╝██║  ██║     ██║"
 echo "╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝    ╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝     ╚═╝"
-echo "${BLUE}--------------------------|Welcome to My script installer|---------------------------${RESET}"
+echo "${BLUE}══════════════════════════║Welcome to My script installer║═══════════════════════════${RESET}"
 echo -e
 echo -e
 echo -e
@@ -34,91 +34,100 @@ echo -e
 echo -e
 echo -e
 echo -e
-echo "${RED}------------|Thank You, Pleas wait....|------------${RESET}"
+echo "${RED}════════════║Thank You, Please wait....║════════════${RESET}"
 echo -e
 echo -e
 echo -e
 echo -e
-# sleep 2
+sleep 2
 
-if [ $update = 'y' ]
+
+if [ "$update" == "y" ]
 then
   echo "${RED}Updating The System${RESET}"
-  # sudo apt-get update -y
+  sudo apt-get update -y
 fi
-if [ $os = 'y' ]
+if [ "$os" = 'y' ]
 then
   if ! [ -x "$(command -v ssh)" ]
   then
       echo "${GREEN}Installing ${BLUE}openssh${RESET}"
-      # sudo apt install openssh-server -y
+      sudo apt install openssh-server -y
   else
-    echo "${BLUE}openssh${RED} is Installed${RESET}"
+    echo "${BLUE}openssh${RED} already Installed${RESET}"
   fi
 fi
-if [ $git = 'y' ]
+if [ "$git" = 'y' ]
 then
   if ! [ -x "$(command -v git)" ]
   then
     echo "${RED}Installing ${BLUE}git${RESET}"
-    # sudo apt install git -y
+    sudo apt install git -y
   else
-    echo "${BLUE}git${RED} is Installed${RESET}"
+    echo "${BLUE}git${RED} already Installed${RESET}"
   fi
 fi
-if [ $curl = 'y' ]
+if [ "$curl" = 'y' ]
 then
   if ! [ -x "$(command -v curl)" ]
   then
     echo "${RED}Installing ${BLUE}curl${RED}, ${BLUE}ca-certificates${RED} and ${BLUE}gnupg${RESET}"
-    # sudo apt-get install ca-certificates curl gnupg lsb-release -y
+    sudo apt-get install ca-certificates curl gnupg lsb-release -y
   else
-    echo "${BLUE}curl${RED} is Installed${RESET}"
+    echo "${BLUE}curl${RED} already Installed${RESET}"
   fi
 fi
-if [ $curl = 'y' ]
+if [ "$curl" = 'y' ]
 then
   if ! [ -x "$(command -v lsb_release)" ]
   then
     echo "${RED}Installing ${BLUE}lsb-release${RESET}"
-    # sudo apt-get install lsb-release -y
+    sudo apt-get install lsb-release -y
   else
-    echo "${BLUE}lsb_release${RED} is Installed${RESET}"
+    echo "${BLUE}lsb_release${RED} already Installed${RESET}"
   fi
 fi
-if [ $docker = 'y' ]
+if [ "$docker" = 'y' ]
 then
   if ! [ -x "$(command -v docker)" ]
   then
     echo "${RED}Adding ${BLUE}Docker${RED}’s official ${BLUE}GPG key${RESET}"
-    # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg ║ sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "${RED}setting up the stable repository${RESET}"
-    # echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-    #   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" ║ sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     echo "${RED}Re-update The System${RESET}"
-    # sudo apt-get update -y
+    sudo apt-get update -y
     echo "${RED}Installing ${BLUE}docker${RESET}"
-    # sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
     else
-      echo "${BLUE}docker${RED} is Installed${RESET}"
+      echo "${BLUE}docker${RED} already Installed${RESET}"
     fi
 fi
-if [ $dsudo = 'y' ]
+if [ "$dsudo" = 'y' ]
 then
   echo "${RED}Make docker run as${BLUE} root${RESET}"
-  # sudo groupadd docker
-  # sudo usermod -aG docker $USER
-  # sudo service docker restart
+  if grep -q docker /etc/group
+  then
+    echo "${GREEN}The Group${BLUE} docker${GREEN} Exists${RESET}"
+  else
+    sudo groupadd docker
+  fi
+  sudo usermod -aG docker $USER
+  sudo service docker restart
+  echo -e "${RED}╔════════════════════════════║NOTE:║════════════════════════╗${RESET}"
+  echo -e "${RED}║   ${BLUE} Please Restart Your machine To apply Those changes!${RED}   ║${RESET}"
+  echo -e "${RED}╚═══════════════════════════════════════════════════════════╝${RESET}"
 fi
-if [ $dc = 'y' ]
+if [ "$dc" = 'y' ]
 then
   if ! [ -x "$(command -v docker-compose)" ]
   then
     echo "${RED}Downloading the current stable release of Docker Compose${RESET}"
-    # sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     echo "${RED}Apply executable permissions to the binary${RESET}"
-    # sudo chmod +x /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
   else
-    echo "${BLUE}docker-compose${RED} is Installed${RESET}"
+    echo "${BLUE}docker-compose${RED} already Installed${RESET}"
   fi
 fi
